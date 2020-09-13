d04a9726b10b4ec6e3c3831b7a1de2d3b66771d4
CSamarakoon <CSamarakoon@5CG7414N9F>
2020-09-13 15:12:34 +1200

    Add project files.

---
+++ Chanuri_SignalR/API/RealTimeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Chanuri_SignalR.hub;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Chanuri_SignalR.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RealTimeController : ControllerBase
+    {
+        private readonly IHubContext<ChatHub> _hub;
+        // GET: api/RealTime
+        
+       public RealTimeController(IHubContext<ChatHub> hub)
+        {
+            _hub = hub;
+        }
+
+        // POST: api/RealTime
+        [HttpPost]
+        public void Post(List<string> data)
+        {
+            foreach (var item in data)
+            {
+                _hub.Clients.All.SendAsync(item);
+            }
+            
+
+        }
+
+        // PUT: api/RealTime/5
+       
+    }
+}
